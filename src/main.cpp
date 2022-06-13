dd3d166c30e0a9af2f5f898b431a3c08c358f747
Heisenberg <el_bueno222@hotmail.com>
2022-06-13 20:27:49 +0100

    Compiling version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+/**
+ * This example turns the ESP32 into a Bluetooth LE mouse that continuously moves the mouse.
+ */
+#include <BleMouse.h>
+
+//BleMouse bleMouse;
+BleMouse bleMouse("Unicorn mouse", "Unicorn enterprise", 100);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleMouse.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(bleMouse.isConnected()) {
+
+    Serial.println("Move mouse pointer up");
+      bleMouse.move(0,-1);
+    //digitalWrite(LED_BUILTIN, HIGH); 
+    delay(1000);
+
+    Serial.println("Move mouse pointer down");
+      bleMouse.move(0,1);
+    //digitalWrite(LED_BUILTIN, LOW);  
+
+    delay(60000);
+
+  }
+  else {
+    Serial.println("BLE not connected");
+    delay(1000);
+  }
 }
