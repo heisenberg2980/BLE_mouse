be8a00a4bd75489eaa677dbf4576a2530344e96f
Heisenberg <el_bueno222@hotmail.com>
2022-06-16 12:02:27 +0100

    Restart after one minute not connected

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,43 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
   bleMouse.begin();
+	//pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+
+  static long mouseTick = millis();
+  static long resetTick = millis();
+
   if(bleMouse.isConnected()) {
 
     Serial.println("Move mouse pointer up");
-      bleMouse.move(0,-1);
-    //digitalWrite(LED_BUILTIN, HIGH); 
+    bleMouse.move(0,-1);
+    //digitalWrite(LED_BUILTIN, LOW); 
     delay(1000);
 
     Serial.println("Move mouse pointer down");
-      bleMouse.move(0,1);
-    //digitalWrite(LED_BUILTIN, LOW);  
+    bleMouse.move(0,1);
+    //digitalWrite(LED_BUILTIN, HIGH);  
 
     delay(60000);
+    mouseTick = millis();
+    resetTick = millis();
 
   }
   else {
-    Serial.println("BLE not connected");
-    delay(1000);
+    //digitalWrite(LED_BUILTIN, LOW); 
+    //delay(60000);
+    if (millis() - mouseTick > 10000) {
+      mouseTick = millis();
+      Serial.println("BLE not connected");
+      if (millis() - resetTick > 60000) {
+        resetTick = millis();
+     		Serial.println("Restarting");
+    		ESP.restart();
+      }
+    }
+    //digitalWrite(LED_BUILTIN, HIGH);  
+    //delay(120000);
   }
 }
